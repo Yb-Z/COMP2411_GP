8900ca09b5ed7a33d6fca3fe88942eaea13c914f
Moenupa WANG <mineworldy@outlook.com>
2020-12-08 03:04:49 +0800

    timer added

--- AutoExamSys/SystemModel/views.py
+++ AutoExamSys/SystemModel/views.py
@@ -3,16 +3,8 @@ from SystemModel import models
 from django.http import HttpResponse
 from django.contrib.auth import logout
 # Create your views here.
-'''
 def index(request):
     return render(request,"./index.html")
-'''
-def login(request):
-    return render(request, "./login.html")
-    
-def demo(request):
-    return render(request, "./demo.html")
-
 def studentLogin(request):
     if request.method=='POST':
         # 获取表单信息
@@ -29,8 +21,7 @@ def studentLogin(request):
         if password==student.password:  #登录成功
             return render(request,'./student.html',{'student':student})
         else:
-            return render(request, './student.html', {'message': 'Wrong Password!'})
-
+            return render(request,'./student.html',{'message':'Wrong Password!'})
 def teacherLogin(request):
     if request.method=='POST':
         # 获取表单信息
@@ -43,27 +34,12 @@ def teacherLogin(request):
         if password==teacher.password:  #登录成功
             return render(request,'./teacher.html',{'teacher':teacher})
         else:
-            pass
-
-def logout(request):
-    return render(request, "./login.html")
-'''
-def showQuestion(request):
-    if request.method=='POST':
-        # 获取表单信息
-        #TODO:
-        paper=request.POST.get('paperID')
-        password=request.POST.get('password')
-        print("id",stuId,"password",password)
-    
-        # 通过学号获取该学生实体
-        student=models.Student.objects.get(id=stuId)
-        print(student)
-        if password==student.password:  #登录成功
-            return render(request,'./student.html',{'student':student})
-        else:
-            return render(request, './student.html', {'message': 'Wrong Password!'})
-
-def nextQuestion(request):
-    pass
-'''
+            return render(request, './teacher.html', {'message': 'Wrong Password!'})
+            
+def getExamPaper(request):
+    if request.method == 'POST':
+        # TODO:求补全& fix 这里 下一行的 time 是错误的！
+        time = request.POST.get('time')  # time: a string HH:MM:SS
+        timeInSec = 3600 * int(time[0:2]) + 60 * int(time[3:5]) + int(time[6:])  # 考试时间转成秒数
+        data = {'timeInSec': timeInSec} # TODO: 还有，记得加
+        return render(request, './answer.html', data)
