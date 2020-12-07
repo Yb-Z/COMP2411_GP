2aeb3c5f742ffc5223d3a36ca9f9bb9e3e040953
Moenupa WANG <mineworldy@outlook.com>
2020-12-07 23:08:13 +0800

    html update

--- AutoExamSys/SystemModel/views.py
+++ AutoExamSys/SystemModel/views.py
@@ -8,7 +8,10 @@ def index(request):
     return render(request,"./index.html")
 '''
 def login(request):
-    return render(request,"./login.html")
+    return render(request, "./login.html")
+    
+def demo(request):
+    return render(request, "./demo.html")
 
 def studentLogin(request):
     if request.method=='POST':
@@ -26,7 +29,8 @@ def studentLogin(request):
         if password==student.password:  #登录成功
             return render(request,'./student.html',{'student':student})
         else:
-            return render(request,'./student.html',{'message':'Wrong Password!'})
+            return render(request, './student.html', {'message': 'Wrong Password!'})
+
 def teacherLogin(request):
     if request.method=='POST':
         # 获取表单信息
