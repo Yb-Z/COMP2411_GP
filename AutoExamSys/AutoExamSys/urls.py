6e7384274de562b78db46e094689ea42432751d1
toolsmax0 <xingshijichina@outlook.com>
2020-12-08 15:28:11 +0800

    import modified

--- AutoExamSys/AutoExamSys/urls.py
+++ AutoExamSys/AutoExamSys/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from SystemModel import views
+from AutoExamSys.SystemModel import views
 
 urlpatterns = [
     #管理员登陆
